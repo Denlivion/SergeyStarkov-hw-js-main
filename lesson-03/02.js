bf83aa85b3e1a9e44a4a3fbc72f58b0b97e3e0a4
Denlivion <dinegromoff@gmail.com>
2026-02-12 22:15:53 +0300

    les3-2nd-task-done

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(randomNum) {
+    let evenNum = (randomNum % 2 === 0)?true:false;
+    console.log(evenNum);
+    return (evenNum)
+}
+isEven(0)
