38f963ab1c24601988b22d85ce6e14e8c609f9ac
Denlivion <dinegromoff@gmail.com>
2026-02-12 20:39:50 +0300

    lesson3-1st-task-done

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,14 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 function calculateAverage(num1, num2) {
-    if ((num1 > 0) && (num2 > 0))
-    {
-      let validateNum =  (num1 + num2)/2;
-      console.log(validateNum)
+    (num1 + num2)/2;
+      console.log((num1 + num2)/2)
         return ((num1 + num2)/2)
-    } else {
-        console.log('Пожалуйста, введи два произвольных числа выше нуля!')
-        return ('Пожалуйста, введи два произвольных числа выше нуля!')
-    }
 }
 
-calculateAverage(0, 20)
+calculateAverage(-5, 20)
