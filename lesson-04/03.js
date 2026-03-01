88912516a11d7f4cebe80493cdfc286a1f67bc68
Denlivion <dinegromoff@gmail.com>
2026-03-01 15:13:58 +0300

    less4_3rd-task-done

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `findCommonElements`, которая принимает два массива и возвращает массив, содержащий общие элементы из обоих массивов.
+Напишите функцию `findCommonElements`, которая принимает два массива и возвращает массив,
+содержащий общие элементы из обоих массивов.
 
 Входные данные:
 
@@ -14,7 +15,38 @@
 Пример использования:
 findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 
-Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее.
+Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+const someArray1 = [0, 2, 43, 54, 5, 81, 9, 21, 32]
+const someArray2 = [22, 3, 2, 3, 8, 1, 4, 7, 15, 33]
+
+
+function findCommonElements(array1, array2) {
+
+    let commonElementsArray = [];
+
+    for (let i = 0; i < array1.length; i++){
+        let elementToCompare = array1[i];
+        if((includesElement(array1, elementToCompare)) === (includesElement(array2, elementToCompare))){
+            commonElementsArray.push(elementToCompare)
+        }
+    }
+    console.log(commonElementsArray)
+    return commonElementsArray
+}
+
+findCommonElements(someArray1, someArray2)
+
+function includesElement(array, element) {
+    let isInArray = false
+    for (let i = 0; i < array.length; i++) {
+        let argument = array[i];
+        if (argument === element) {
+            isInArray = true
+            console.log(argument)
+        }
+    }
+    return isInArray
+}
