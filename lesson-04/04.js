cb72acf112db32ead190d00e86fe645aedc11ad1
Denlivion <dinegromoff@gmail.com>
2026-03-01 20:55:49 +0300

    less4_4th-task-done

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку,
+ где каждый символ из исходной строки повторяется дважды.
 
 Входные данные:
 
@@ -11,5 +12,20 @@
 
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
+let stringArray
+let doubledString
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(someString) {
+    let permanentArray = [];
+    stringArray = someString.split('');
+    for (let i = 0; i < stringArray.length; i++){
+        permanentArray.push(stringArray[i])
+        permanentArray.push(stringArray[i])
+        doubledString = permanentArray.join('')
+    }
+
+    console.log(doubledString)
+    return doubledString
+}
+
+doubleEachCharacter('o-lo-lo',)
