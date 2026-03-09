0c8207d279bc9d175e24b9aa9cdff8ab39eb51ee
Denlivion <dinegromoff@gmail.com>
2026-03-09 20:50:15 +0300

    less5-2nd-task-done

---
+++ lesson-05/02.js
@@ -0,0 +1,55 @@
+/*
+Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства
+ в виртуальной галерее. Функция должна принимать три параметра:
+
+- объект галереи
+- название произведения (ключ)
+- новое значение
+
+Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено.
+Если произведения нет, оно должно быть добавлено в объект галерею.
+
+Пример использования функции:
+
+const gallery = {
+  'Mona Lisa': 'Leonardo da Vinci',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch'
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
+
+Ожидаемый вывод:
+{
+  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch',
+  'The Persistence of Memory': 'Salvador Dali'
+}
+*/
+
+const gallery = {
+  'Mona Lisa' : 'Leonardo da Vinci',
+  'Starry Night' : 'Vincent van Gogh',
+  'The Scream' : 'Edvard Munch',
+}
+
+function updateGallery(galleryObject, masterpieceName, newValue) {
+    for (const key in galleryObject) {
+        if (key === masterpieceName) {
+            galleryObject[key] = newValue
+        }
+        else
+        {
+            galleryObject[masterpieceName]= newValue;
+        }
+    }
+    return gallery
+}
+updateGallery(gallery, 'Some Masterpiece', 'Some Artist')
+updateGallery(gallery, 'Some Masterpiece', 'Some Artist, 1222222')
+
+console.log(gallery)
