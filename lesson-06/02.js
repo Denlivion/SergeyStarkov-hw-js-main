e76d37a1d969a8dc6211da597caa2f2fe3ea4960
Denlivion <dinegromoff@gmail.com>
2026-03-17 12:01:31 +0300

    less6-2nd-task-redone1

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -23,7 +23,7 @@ const doubledNumbers = map(numbers, (element, index) => {
 
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
-const numbers = ['1', '2', '3', '4', '5']
+const numbers = [1, 2, 3, 4, 5]
 
 const callback = ((element, index) => {
     return element + 10
